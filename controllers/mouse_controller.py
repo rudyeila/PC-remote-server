f73fa1a060f9b49a1ab9079a7547e0ab4b103802
s353235 <rudy.ailabouni@stud-mail.uni-wuerzburg.de>
2020-09-21 15:13:40 +0200

    now supports remote clicking

--- controllers/mouse_controller.py
+++ controllers/mouse_controller.py
@@ -4,7 +4,6 @@ from typing import Tuple
 import mouse
 
 
-
 class MouseController:
 
     def get_position(self) -> Tuple[int, int]:
@@ -17,8 +16,21 @@ class MouseController:
         """
         return mouse.get_position()
 
-
     def move_mouse(self, x: int, y: int, absolute=True, duration=0):
         mouse.move(x, y, absolute, duration)
 
-    
+    def perform_action(self, action: str): # Change action type from string to predefined costants
+        if (action == "LEFT_CLICK"):
+            mouse.click()
+            return True
+        elif (action == "RIGHT_CLICK"):
+            mouse.click(mouse.RIGHT)
+            return True
+        elif (action == "MIDDLE_CLICK"):
+            mouse.click(mouse.MIDDLE)
+            return True
+        elif (action == "DOUBLE_CLICK"):
+            mouse.double_click()
+            return True
+        else: 
+            return False
--- routes/mouse_route.py
+++ routes/mouse_route.py
@@ -1,9 +1,8 @@
-
-
 from flask import make_response, request
 from flask_restful import Resource, reqparse
 
 from controllers.mouse_controller import MouseController
+from utils.errors import error_response
 
 
 class Mouse(Resource):
@@ -11,13 +10,20 @@ class Mouse(Resource):
 
     def __init__(self):
         self.__parser = reqparse.RequestParser()
-        self.__parser.add_argument('x', type=int, required=True,
-                                   help='The new X coordinate of the mouse')
-        self.__parser.add_argument('y', type=int, required=True,
-                                   help='The new Y coordinate of the mouse')
-        self.__parser.add_argument(
+        # choices=["MOVE, LEFT_CLICK, RIGHT_CLICK, DOUBLE_CLICK, MIDDLE_CLICK"]
+        self.__parser.add_argument('action', type=str, choices=[
+                                   "MOVE, LEFT_CLICK, RIGHT_CLICK, DOUBLE_CLICK, MIDDLE_CLICK"], required=True, help='The action to perform on the mouse resource')
+
+        self.__move_parser = reqparse.RequestParser()
+        self.__move_parser.add_argument('action', type=str, choices=[
+                                        "MOVE"], default="MOVE", help='The action to perform on the mouse resource')
+        self.__move_parser.add_argument('x', type=int, required=True,
+                                        help='The new X coordinate of the mouse')
+        self.__move_parser.add_argument('y', type=int, required=True,
+                                        help='The new Y coordinate of the mouse')
+        self.__move_parser.add_argument(
             'absolute', default=True, type=bool, help='Should the new mouse coordinates be absolute or relative?')
-        self.__parser.add_argument(
+        self.__move_parser.add_argument(
             'duration', default=0, type=int, help='The duration of the movement animation, if required.')
 
     def get(self):
@@ -28,13 +34,25 @@ class Mouse(Resource):
                 'pos_y': pos_y,
             }}
         except Exception as err:
-            return make_response({'error': str(err)}, 500)
+            return error_response(err, 500)
 
     def put(self):
         try:
-            args = self.__parser.parse_args()
-            print(args)
-            Mouse.mouseController.move_mouse(args.x, args.y, args.absolute, args.duration)
-            return "success", 200
+            body = request.get_json()
+            print(body)
+            if ('action' not in body):
+                return {"error": "Invalid Syntax. You must provide an action to perform on the mouse resource"}, 400
+            action = body['action']
+            if (action == "MOVE"):
+                reqbody = self.__move_parser.parse_args()
+                Mouse.mouseController.move_mouse(
+                    reqbody.x, reqbody.y, reqbody.absolute, reqbody.duration)
+                return self.get()
+
+            result = Mouse.mouseController.perform_action(action)
+            if (result):
+                return {"data": {"action": action, "result": "success"}}, 200
+            return error_response("undefined action", 400)
         except Exception as err:
-            return make_response({'error': str(err)}, 500)
+            print(err)
+            return error_response(err, 500)
