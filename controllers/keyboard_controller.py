e87dfbd1a23f6c78e3aa5acdce3c952b23810f8c
Rudy Ailabouni <eilabouni.rudy@gmail.com>
2020-09-22 11:02:47 +0200

    add docstrings

--- controllers/keyboard_controller.py
+++ controllers/keyboard_controller.py
@@ -1,3 +1,6 @@
+import time
+
+# https://pypi.org/project/keyboard/
 import keyboard
 
 
@@ -6,18 +9,28 @@ def hold(hotkey):
     keyboard.press(hotkey)
 
 
-def press_and_release(hotkey):
+def press_and_release(hotkey: str or int):
     """Sends OS events that perform the given *hotkey* hotkey.
 
     - `hotkey` can be either a scan code (e.g. 57 for space), single key  
     (e.g. 'space') or multi-key, multi-step hotkey (e.g. 'alt+F4, enter').
 
-    - `do_press` if true then press events are sent. Defaults to True.  
-    - `do_release` if true then release events are sent. Defaults to True.  
-
-        send(57)  
-        send('ctrl+alt+del')  
-        send('alt+F4, enter')  
-        send('shift+s')  
+        press_and_release(57)  
+        press_and_release('ctrl+alt+del')  
+        press_and_release('alt+F4, enter')  
+        press_and_release('shift+s')  
     Note: keys are released in the opposite order they were pressed."""
     keyboard.press_and_release(hotkey)
+
+
+def handle_request(args):
+    """This function handles incoming requests to the keyboard resource
+
+    Args:
+        args (object): The HTML request body, which looks as follows: 
+        {"hotkey": str or int, "hold": bool}
+    """
+    if (args.hold):
+        hold(args.hotkey)
+    else:
+        press_and_release(args.hotkey)
