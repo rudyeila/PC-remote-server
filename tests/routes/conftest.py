b8357f21224f38821a489026d253d8af1ce5f71a
Rudy Ailabouni <eilabouni.rudy@gmail.com>
2020-09-24 12:06:48 +0200

    add comment

--- tests/routes/conftest.py
+++ tests/routes/conftest.py
@@ -4,6 +4,9 @@ from server import app as flask_app
 
 """ Sets up the flask app for testing before running each test.  """
 
+
+# https://dev.to/po5i/how-to-add-basic-unit-test-to-a-python-flask-app-using-pytest-1m7a
+
 @pytest.fixture
 def app():
     yield flask_app
