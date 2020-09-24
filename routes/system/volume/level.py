025c6b9dd1d98fd80f9a3b7b8f8a41974b9ec4c8
Rudy Ailabouni <eilabouni.rudy@gmail.com>
2020-09-24 12:06:07 +0200

    update imports following refactoring

--- routes/system/volume/level.py
+++ routes/system/volume/level.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restful import Resource, reqparse
 from utils.errors import error_response
 
-from controllers.volume_controller import VolumeController
+from controllers.volume.volume_controller import VolumeController
 
 
 class Level(Resource):
